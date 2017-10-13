7d957bc5f49cad507d5de6a3e15776598224912e
reachviji <reachvijiarun@gmail.com>
2017-10-13 12:01:58 +0530

    Update rosbridge.js

--- rosbridge.js
+++ rosbridge.js
@@ -1,106 +1,93 @@
-#!/usr/bin/env nodejs 
- 
- 
-var WebSocket = require('ws'); 
-var ROSLIB = require('roslib'); 
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
-const restService = express();  
- 
- 
-restService.use(bodyParser.urlencoded({ 
-	extended: true 
-})); 
- 
-restService.use(bodyParser.json()); 
- 
- 
- 
-restService.post('/echo', function(req, res) {
-	var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.command ? req.body.result.parameters.command : "Seems like some problem. Speak again."
-	//var speech = "heyyyyyyyyy";
-	console.log("This is from webhook: " +speech);
-	senddatatows(speech);
-	
-	return res.json({
-		speech: speech,
-		displayText: speech,
-		source: 'command'
-	});
-	
-});
- 
-function senddatatows(speech) { 
-	/* Load application settings */  
-	var no_skipping_factor = 1; 
+'use strict';
+
+const functions = require('firebase-functions'); // Cloud Functions for Firebase library
+const DialogflowApp = require('actions-on-google').DialogflowApp; // Google Assistant helper library
+const googleAssistantRequest = 'google'; // Constant to identify Google Assistant requests
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+  // An action is a string used to identify what needs to be done in fulfillment
+  let action = request.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
+
+  // Parameters are any entites that Dialogflow has extracted from the request.
+  const parameters = request.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
+
+  // Contexts are objects used to track and store conversation state
+  const inputContexts = request.body.result.contexts; // https://dialogflow.com/docs/contexts
+
+  // Get the request source (Google Assistant, Slack, API, etc) and initialize DialogflowApp
+  const requestSource = (request.body.originalRequest) ? request.body.originalRequest.source : undefined;
+  const app = new DialogflowApp({request: request, response: response});
+
+  // Create handlers for Dialogflow actions as well as a 'default' handler
+  const actionHandlers = {
+    // The default welcome intent has been matched, welcome the user (https://dialogflow.com/docs/events#default_welcome_intent)
+    'input.welcome': () => {
+      // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
+        sendResponse('Hello, Welcome to my voice command agent! You can use me to command a robot'); // Send simple response to user
+    },
+    // Default handler for unknown or undefined actions
+    'default': () => {
+      var userinput = request.body.result.parameters.command;
+      senddatatows(userinput);
+      // Use the Actions on Google lib to respond to Google requests; for other requests use JSON
+      let responseToUser = {
+          //richResponses: richResponses, // Optional, uncomment to enable
+          //outputContexts: [{'name': 'weather', 'lifespan': 2, 'parameters': {'city': 'Rome'}}], // Optional, uncomment to enable
+         speech: request.body.result.parameters.command, // spoken response
+         displayText: request.body.result.parameters.command // displayed response
+        };
+      sendResponse(responseToUser);
+    }
+  };
+
+  // If undefined or unknown action use the default handler
+  if (!actionHandlers[action]) {
+    action = 'default';
+  }
+
+  // Run the proper handler function to handle the request from Dialogflow
+  actionHandlers[action]();
+
+  // Function to send correctly formatted responses to Dialogflow which are then sent to the user
+  function sendResponse (responseToUser) {
+    // if the response is a string send it as a response to the user
+    if (typeof responseToUser === 'string') {
+      let responseJson = {};
+      responseJson.speech = responseToUser; // spoken response
+      responseJson.displayText = responseToUser; // displayed response
+      response.json(responseJson); // Send response to Dialogflow
+    } else {
+      // If the response to the user includes rich responses or contexts send them to Dialogflow
+      let responseJson = {};
+
+      // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
+      responseJson.speech = responseToUser.speech || responseToUser.displayText;
+      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+
+      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+      responseJson.data = responseToUser.richResponses;
+
+      // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      responseJson.contextOut = responseToUser.outputContexts;
+
+      response.json(responseJson); // Send response to Dialogflow
+    }
+  }
+  
+  //send the data
+  function senddatatows(speech) { 
 	/*ROS-bridge endpoint*/ 
-	
-	var ros = new ROSLIB.Ros({
-		url : 'ws://192.168.198.131:9090'
-	});
-	
+	var WebSocket = require('ws'); 
+
 	/*webSocket Cloud options*/ 
-	var ws_client_state = "offline"; 
 	var ws = new WebSocket('wss://ws-broadcast-server-d062507.cfapps.eu10.hana.ondemand.com'); 
  
 	ws.on('open', function open() { 
-		ws_client_state = "connected";
-		console.log("connected");
+		console.log("connected to HANA websocket");
 		ws.send(speech); 
 	}); 
- 
-	ws.on('message', function incoming(msg) { 
-		console.log("This is from websocket: " +msg); 
-	}); 
- 
-	ws.on('close', function close() { 
-  //ws_client_state = "offline"; 
-		console.log('websocket cloud client disconnected'); 
-	}); 
- 
-	 
-	/* 
-	ROS-BRIDGE HANDLDER 
-	*/ 
-	/*State of ROS-BRIDGE connection*/ 
- 
-	var ros_bridge_state = 'offline'; 
- 
-	ros.on('connection', function() { 
-	  ros_bridge_state = "Connected"; 
-	  console.log('Connected to ROS-Bridge server.'); 
-	}); 
- 
- 
-	ros.on('error', function(error) { 
-	  console.log('Error connecting to ROS-bridge server: ', error); 
-	  ros_bridge_state = 'offline' 
-	}); 
- 
-	ros.on('close', function() { 
-	  console.log('Connection to ROS-Bridge server closed.'); 
-	  ros_bridge_state = 'offline' 
-	}); 
-	
-	// Publishing a Topic
-// ------------------
-
-	var speak = new ROSLIB.Topic({
-	  ros : ros,
-	  name : '/chatbot',
-	  messageType : 'std_msgs/String'
-	});
-
-	var str = new ROSLIB.Message({
-			data: speech
-	});
-
-	console.log("Publishing chatbot");
-	speak.publish(str);
-	 
- 
-}
-
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
+  }
 });
